b242df9f81423d443f3cda9f7624143ff7a9c904
zunema <yxxa.abc@gmail.com>
2023-01-13 00:09:47 +0900

    土台作成

---
+++ src/test/java/com/example/demo_sample/DemoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
